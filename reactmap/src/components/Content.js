13bb52e6e76272bb013806217263e7347e450858
johannyberg <johan.k.nyberg@gmail.com>
2021-05-06 13:25:22 +0300

    ScrollWeelZoom true

--- reactmap/src/components/Content.js
+++ reactmap/src/components/Content.js
@@ -13,7 +13,7 @@ const Content = () => {
     
     return (
        <div className="content">
-           <MapContainer center={center} zoom={13} scrollWheelZoom={false}>
+           <MapContainer center={center} zoom={13} scrollWheelZoom={true}>
                 <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
